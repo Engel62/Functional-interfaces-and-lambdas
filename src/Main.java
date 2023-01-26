92c9079f8f3aafe325a42936ba96513c60931770
Engel62 <sr.engel@bk.ru>
2023-01-26 13:51:53 +0300

    1-4 выполненно

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
+import java.util.Random;
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Predicate;
+import java.util.function.Supplier;
 
 public class Main {
     public static void main(String[] args) {
@@ -31,8 +34,33 @@ public class Main {
 
         System.out.println("____________________________________-");
 
+        Function <Double, Long> roundLong = new Function<Double, Long>() {
+            @Override
+            public Long apply(Double aDouble) {
+                return Math.round(aDouble);
+            }
+        };
+        System.out.println(roundLong.apply(16.84));
+        Function<Double, Long> roundLong1 = aDouble -> Math.round(aDouble);
+        System.out.println(roundLong1.apply(17.96));
+        System.out.println("____________________________________-");
 
-        }
+
+        Supplier <Integer> randomInteger = new Supplier<Integer>() {
+            @Override
+            public Integer get() {
+                Random random = new Random();
+                return random.nextInt(100);
+            }
+        };
+        System.out.println(randomInteger.get());
+        Supplier<Integer> randomInteger1 = () -> {
+            Random random = new Random();
+            return random.nextInt(100);
+        };
+        System.out.println(randomInteger1.get());
+
+    }
 
     }
 
